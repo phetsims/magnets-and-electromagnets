312119c018522034c12d252f5d3cb992cae96ef1
pixelzoom <cmalley@pixelzoom.com>
2024-03-13 17:13:10 -0600

    factor out FELSim, https://github.com/phetsims/faradays-electromagnetic-lab/issues/101

--- js/magnets-and-electromagnets-main.ts
+++ js/magnets-and-electromagnets-main.ts
@@ -6,16 +6,13 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import MagnetsAndElectromagnetsStrings from './MagnetsAndElectromagnetsStrings.js';
-import FELConstants from '../../faradays-electromagnetic-lab/js/common/FELConstants.js';
-import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 import BarMagnetScreen from '../../faradays-electromagnetic-lab/js/bar-magnet/BarMagnetScreen.js';
-import FELPreferencesNode from '../../faradays-electromagnetic-lab/js/common/view/preferences/FELPreferencesNode.js';
 import ElectromagnetScreen from '../../faradays-electromagnetic-lab/js/electromagnet/ElectromagnetScreen.js';
 import FELPreferences from '../../faradays-electromagnetic-lab/js/common/model/FELPreferences.js';
+import FELSim from '../../faradays-electromagnetic-lab/js/common/FELSim.js';
 
 // If addEarthCheckbox query parameter was not in the URL, change the default.
 if ( !QueryStringMachine.containsKey( 'addEarthCheckbox' ) ) {
@@ -23,31 +20,11 @@ if ( !QueryStringMachine.containsKey( 'addEarthCheckbox' ) ) {
 }
 
 simLauncher.launch( () => {
-
   const titleStringProperty = MagnetsAndElectromagnetsStrings[ 'magnets-and-electromagnets' ].titleStringProperty;
-
   const screens = [
     new BarMagnetScreen( Tandem.ROOT.createTandem( 'barMagnetScreen' ) ),
     new ElectromagnetScreen( Tandem.ROOT.createTandem( 'electromagnetScreen' ) )
   ];
-
-  const options: SimOptions = {
-    credits: FELConstants.CREDITS,
-    phetioDesigned: true,
-    preferencesModel: new PreferencesModel( {
-      visualOptions: {
-        supportsProjectorMode: true
-      },
-      simulationOptions: {
-        customPreferences: [ {
-          createContent: tandem => new FELPreferencesNode( {
-            tandem: tandem.createTandem( 'simPreferences' )
-          } )
-        } ]
-      }
-    } )
-  };
-
-  const sim = new Sim( titleStringProperty, screens, options );
+  const sim = new FELSim( titleStringProperty, screens );
   sim.start();
 } );
