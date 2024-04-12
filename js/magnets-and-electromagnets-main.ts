0c393881da4a921d56d206d1791dc7d50e8bd611
pixelzoom <cmalley@pixelzoom.com>
2024-04-12 17:42:01 -0600

    instrument Preferences Properties only when they are relevant, https://github.com/phetsims/faradays-electromagnetic-lab/issues/160

--- js/magnets-and-electromagnets-main.ts
+++ js/magnets-and-electromagnets-main.ts
@@ -13,18 +13,21 @@ import BarMagnetScreen from '../../faradays-electromagnetic-lab/js/bar-magnet/Ba
 import ElectromagnetScreen from '../../faradays-electromagnetic-lab/js/electromagnet/ElectromagnetScreen.js';
 import FELPreferences from '../../faradays-electromagnetic-lab/js/common/model/FELPreferences.js';
 import FELSim from '../../faradays-electromagnetic-lab/js/common/FELSim.js';
+import FELQueryParameters from '../../faradays-electromagnetic-lab/js/common/FELQueryParameters.js';
 
-// If addEarthCheckbox query parameter was not in the URL, change the default.
-if ( !QueryStringMachine.containsKey( 'addEarthCheckbox' ) ) {
-  FELPreferences.addEarthCheckboxProperty.value = true;
-}
+// The 'Earth' checkbox should be visible by default, unless specified otherwise in the URL.
+// See https://github.com/phetsims/faradays-electromagnetic-lab/issues/23
+const addEarthCheckbox = QueryStringMachine.containsKey( 'addEarthCheckbox' ) ? FELQueryParameters.addEarthCheckbox : true;
 
 simLauncher.launch( () => {
   const titleStringProperty = MagnetsAndElectromagnetsStrings[ 'magnets-and-electromagnets' ].titleStringProperty;
+  const preferences = new FELPreferences( {
+    addEarthCheckbox: addEarthCheckbox
+  } );
   const screens = [
-    new BarMagnetScreen( Tandem.ROOT.createTandem( 'barMagnetScreen' ) ),
-    new ElectromagnetScreen( Tandem.ROOT.createTandem( 'electromagnetScreen' ) )
+    new BarMagnetScreen( preferences, Tandem.ROOT.createTandem( 'barMagnetScreen' ) ),
+    new ElectromagnetScreen( preferences, Tandem.ROOT.createTandem( 'electromagnetScreen' ) )
   ];
-  const sim = new FELSim( titleStringProperty, screens );
+  const sim = new FELSim( titleStringProperty, screens, preferences );
   sim.start();
 } );
