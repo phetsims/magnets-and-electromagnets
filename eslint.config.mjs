8697538db867a3f163088dfb08ac148b3f0d46cb
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:14 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for magnets-and-electromagnets.
+ * ESLint configuration for magnets-and-electromagnets.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
